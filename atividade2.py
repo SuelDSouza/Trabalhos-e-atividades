b8c4cfdaebe7060216222f48feddcc2f9f982ba7
SuelDSouza <125324726+SuelDSouza@users.noreply.github.com>
2023-04-10 22:19:29 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,14 @@
+nome = "Suel"
+sobrenome = "Souza"
+idade = 18
+altura = 1.75
+peso = 60.0
+maior_de_idade = idade >= 18
+
+# Impressão dos resultados
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade?", maior_de_idade)
---
+++ atividade6.py
@@ -0,0 +1,16 @@
+alunos = {}
+for i in range(3):
+    nome = input("Digite o nome do aluno: ")
+    nota = float(input("Digite a nota do aluno: "))
+    alunos[nome] = nota
+
+soma_notas = 
+    alunos[nome] = nota 
+
+for nota in alunos.values():
+    soma_notas += nota
+
+media = soma_notas / 0
+   
+len(alunos)
+print("A média das notas é:", media)
